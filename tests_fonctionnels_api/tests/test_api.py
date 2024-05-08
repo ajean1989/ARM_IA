610539e75010a622d0aa99716a22751c20fbf4a3
adrien <adrien.jean1989@gmail.com>
2024-05-08 14:16:32 +0200

    split docker compose prod / dev

--- tests_fonctionnels_api/tests/test_api.py
+++ tests_fonctionnels_api/tests/test_api.py
@@ -14,14 +14,14 @@ headers = {'X-API-Key': list(API_KEYS.keys())[0]}
 
 
 def test_helloworld_traefik() :
-    response = httpx.get(f"http://traefik/api-ia/", headers = headers)
+    response = httpx.get(f"https://traefik/api-ia/", headers = headers, verify=False)
     assert response.status_code == 200
 
 
 def test_helloworld_mlflow() :
-    response = httpx.get(f"http://traefik/mlflow/", headers = headers)
+    response = httpx.get(f"https://traefik/mlflow/", headers = headers, verify=False)
     assert response.status_code == 200
 
 def test_helloworld_gradio() :
-    response = httpx.get(f"http://traefik/gradio/", headers = headers)
+    response = httpx.get(f"https://traefik/gradio/", headers = headers, verify=False)
     assert response.status_code == 200
