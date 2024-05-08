5db1954c1c66e5bc63c2b78d652aef24832391b4
adrien <adrien.jean1989@gmail.com>
2024-05-08 15:33:04 +0200

    split docker compose prod / dev

--- run/build.sh
+++ run/build.sh
@@ -9,4 +9,8 @@ ls
 cd mlflow
 docker compose -f compose.yml -f compose.prod.yml up --build -d --force-recreate
 cd ..
+
+cd gradio
+docker compose -f compose.yml -f compose.prod.yml up --build -d --force-recreate
+cd ..
 # pas de container pour automatic dataset (local)
