c7eb21299b64569a943d7c049b067682e3d4afe1
ajean1989 <105272808+ajean1989@users.noreply.github.com>
2024-05-22 05:41:17 +0200

    add endpoint /api/ for mlflowapi in traefik

--- training/code/train.py
+++ training/code/train.py
@@ -5,24 +5,27 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
 
-    # Load a pretrained YOLO model (recommended for training)
-    model = YOLO('yolov8n.pt')
+    # Set tracking server
+    mlflow.set_tracking_uri(uri="https://jacquenet.com/mlflow")
+    print("URI du serveur de suivi MLflow :", mlflow.get_tracking_uri())
 
-    # Train the model using the 'coco128.yaml' dataset for 3 epochs
-    results = model.train(data=os.path.join("code", 'test_dataset.yml'), epochs=1, batch=-1, device='0')
+    mlflow.set_experiment("Test")
+    print("Expérience actuelle MLflow :", mlflow.get_experiment_by_name("Test"))
 
-    # Evaluate the model's performance on the validation set
-    results = model.val()
-
-    # Export the model to ONNX format
-    success = model.export(format='onnx')
+    
 
     with mlflow.start_run():
 
         mlflow.autolog()
 
-        # Set tracking server
-        mlflow.set_tracking_uri(uri="https://jacquenet.com/mlflow/")
+        # Load a pretrained YOLO model (recommended for training)
+        model = YOLO('yolov8n.pt')
 
-        # Experiment
-        mlflow.set_experiment("Test")
+        # Train the model using the 'coco128.yaml' dataset for 3 epochs
+        results = model.train(data=os.path.join("code", 'test_dataset.yml'), epochs=1, batch=-1, device='0')
+
+        # Evaluate the model's performance on the validation set
+        results = model.val()
+
+        # Export the model to ONNX format
+        success = model.export(format='onnx')
